b72d90cbc542faf4474d0a89c41080dbad8fc568
Charles Durham <cpdurham@gmail.com>
2018-12-06 13:01:45 -0500

    added comments, cleaned

--- app/Main.hs
+++ app/Main.hs
@@ -18,12 +18,15 @@ main = do
     Just (serial,dim,producer) ->
       do
         putStrLn $ "Camera Serial: " ++ show serial
+        putStrLn $ "Running at dimension: " ++ show dim
         runEffect $ producer >-> Pipes.take 20 >-> consumer
 
+
+-- |consumer just consumes frames
+-- and prints the sum of all the pixels mod 255 (Word8 overflow)
 consumer :: Consumer (S.Vector Word8) IO ()
 consumer =
   forever $
   do
     v <- await
-    liftIO $ print $ S.length v
     Pipes.lift $ print $ S.sum v
